5f0c03b6dc01fee6764261f20650c75d1875deb0
Asourcious <barronpm@gmail.com>
2017-12-04 20:32:27 -0500

    Added project files

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.barronpm.sjgf {
+    requires slf4j.api;
+
+    requires logback.classic;
+    requires org.lwjgl;
+    requires org.lwjgl.glfw;
+    requires org.lwjgl.openal;
+    requires org.lwjgl.opengl;
+    requires org.lwjgl.system.jemalloc;
+}
