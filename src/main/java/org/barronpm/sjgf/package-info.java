2c315df17b99a1e3db1fcc74783077a8198e0bc5
Patrick Barron <barronpm@gmail.com>
2017-12-13 13:29:30 -0500

    Added package-info for org.barronpm.sjgf

---
+++ src/main/java/org/barronpm/sjgf/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Base package for SJGF.
+ *
+ * Includes the components required to create a game window.
+ *
+ * @author Patrick Barron
+ * @see org.barronpm.sjgf.draw
+ * @see org.barronpm.sjgf.exceptions
+ * @see org.barronpm.sjgf.input
+ * @see org.barronpm.sjgf.math
+ * @since 1.0
+ */
+package org.barronpm.sjgf;
