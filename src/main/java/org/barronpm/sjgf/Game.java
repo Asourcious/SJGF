e04cdaf7cbc7ffa0052233f5ad884f7239fcd35b
Patrick Barron <barronpm@gmail.com>
2018-01-09 15:47:58 -0500

    Added documentation for the Game interface

--- src/main/java/org/barronpm/sjgf/Game.java
+++ src/main/java/org/barronpm/sjgf/Game.java
@@ -19,10 +19,52 @@ package org.barronpm.sjgf;
 import org.barronpm.sjgf.draw.Color;
 import org.barronpm.sjgf.draw.Graphics;
 
+/**
+ * Game objects control all of the logic needed for a game.
+ *
+ * @author Patrick Barron
+ * @see GameWindow
+ * @since 1.0
+ */
 public interface Game extends Disposable {
+
+    /**
+     * Returns the background color of this game.
+     *
+     * @return the background color of this game
+     */
     Color getBackgroundColor();
 
+    /**
+     * This method is called once during the game's lifecycle before the game loop begins.
+     * Common uses of this method include loading resources to be
+     *
+     * @param window the window associated with this game
+     * @see #dispose()
+     * @since 1.0
+     */
     void init(GameWindow window);
+
+    /**
+     * This method is called when the application should perform a "step"
+     * in its game logic.
+     *
+     * @param window the window associated with this game
+     * @param delta the amount of time that has passed, in seconds, since the last frame
+     * @see #render(GameWindow, Graphics)
+     * @since 1.0
+     */
     void update(GameWindow window, double delta);
+
+    /**
+     * This method is called when the application should render a frame.
+     * Although game logic such as physics is allowed here, it is recommended to
+     * use {@link #update(GameWindow, double)} for this.
+     *
+     * @param window the window associated with this game
+     * @param graphics a graphics object to be used for rendering
+     * @see #update(GameWindow, double)
+     * @since 1.0
+     */
     void render(GameWindow window, Graphics graphics);
 }
