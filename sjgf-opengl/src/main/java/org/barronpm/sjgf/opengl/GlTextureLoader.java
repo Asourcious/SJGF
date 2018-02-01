36ebb6348579eddf61d54ea880807293cf3e0806
Patrick Barron <barronpm@gmail.com>
2018-01-31 21:09:43 -0500

    Changed BufferUtils usage to MemoryStack in GlTextureLoader

--- sjgf-opengl/src/main/java/org/barronpm/sjgf/opengl/GlTextureLoader.java
+++ sjgf-opengl/src/main/java/org/barronpm/sjgf/opengl/GlTextureLoader.java
@@ -19,7 +19,7 @@ package org.barronpm.sjgf.opengl;
 import org.barronpm.sjgf.ResourceLoader;
 import org.barronpm.sjgf.draw.Texture;
 import org.barronpm.sjgf.opengl.draw.GlTexture;
-import org.lwjgl.BufferUtils;
+import org.lwjgl.system.MemoryStack;
 
 import java.io.File;
 import java.nio.ByteBuffer;
@@ -33,27 +33,22 @@ import static org.lwjgl.stb.STBImage.stbi_set_flip_vertically_on_load;
 public class GlTextureLoader implements ResourceLoader<Texture> {
 
     private final ExecutorService threadPool = Executors.newFixedThreadPool(10);
-    private final IntBuffer x;
-    private final IntBuffer y;
-    private final IntBuffer channels;
 
     public GlTextureLoader() {
         stbi_set_flip_vertically_on_load(true);
-        x = BufferUtils.createIntBuffer(1);
-        y = BufferUtils.createIntBuffer(1);
-        channels = BufferUtils.createIntBuffer(1);
     }
 
     @Override
     public Texture load(File file) {
-        ByteBuffer buffer = stbi_load(file.getPath(),
-                x, y, channels, 0);
+        MemoryStack stack = MemoryStack.stackPush();
+        IntBuffer x = stack.mallocInt(1);
+        IntBuffer y = stack.mallocInt(1);
+        IntBuffer channels = stack.mallocInt(1);
+
+        ByteBuffer buffer = stbi_load(file.getPath(), x, y, channels, 0);
         GlTexture texture = new GlTexture(x.get(), y.get(), channels.get() == 4, buffer);
-        x.clear();
-        y.clear();
-        channels.clear();
+        stack.pop();
 
         return texture;
     }
-
 }
